a49820ffb28e0f23754557338741786782b84b71
objektwerks <objektwerks@runbox.com>
2024-05-24 10:14:34 -0400

    enhanced functions

--- src/main/scala/objektwerks/Functions.scala
+++ src/main/scala/objektwerks/Functions.scala
@@ -11,3 +11,9 @@ def factorial(n: Int, acc: Int = 1): Int =
   n match
     case i if i < 1 => acc
     case _ => factorial(n - 1, acc * n)
+
+@tailrec
+def reverse[A](list: List[A], acc: List[A] = List.empty[A]): List[A] =
+  list match
+    case Nil => acc
+    case head :: tail => reverse(tail, head :: acc)
