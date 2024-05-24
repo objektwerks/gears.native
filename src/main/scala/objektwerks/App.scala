a7660395578a296291db8d54310157a026e1fa31
objektwerks <objektwerks@runbox.com>
2024-05-24 10:26:05 -0400

    enhanced app

--- src/main/scala/objektwerks/App.scala
+++ src/main/scala/objektwerks/App.scala
@@ -23,3 +23,17 @@ private def futures(): Unit =
     )
     .awaitAll
     .foreach(joke => println(s"* futures: $joke"))
+
+private def select(): Unit =
+  Async.blocking:
+    val number = Future( factorial(11) )
+    val list = Future( reverse( List(3, 2, 1) ) )
+    val winner = Async.select(
+      number.handle: n =>
+        // always wins! so is it a race? :)
+        s"async select > factorial of 11 = ${n.get}",
+      list.handle: l =>
+        s"async select > reversed List(3, 2, 1) = ${l.get}"
+
+    )
+    println(s"* $winner")
