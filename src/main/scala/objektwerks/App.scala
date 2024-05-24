df3b5b0468f23969462b3b788519075b98616b77
objektwerks <objektwerks@runbox.com>
2024-05-24 10:22:28 -0400

    added app

---
+++ src/main/scala/objektwerks/App.scala
@@ -0,0 +1,11 @@
+package objektwerks
+
+import gears.async.{Async, Future, Retry, SyncChannel, withTimeout}
+import gears.async.Channel.Closed
+import gears.async.Retry.Delay
+import gears.async.default.given
+
+import scala.concurrent.duration.DurationInt
+
+@main
+def runApp: Unit = ???
