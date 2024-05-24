948a1746701888196334e821d25f33174f48f67b
objektwerks <objektwerks@runbox.com>
2024-05-24 10:19:05 -0400

    enhanced functions

--- src/main/scala/objektwerks/Functions.scala
+++ src/main/scala/objektwerks/Functions.scala
@@ -6,6 +6,11 @@ import scala.annotation.tailrec
 import scala.io.{Codec, Source}
 import scala.util.Using
 
+import sttp.client4.{quickRequest, UriContext}
+import sttp.client4.curl.CurlBackend
+import sttp.client4.Response
+import sttp.model.Uri
+
 @tailrec
 def factorial(n: Int, acc: Int = 1): Int =
   n match
@@ -18,11 +23,17 @@ def reverse[A](list: List[A], acc: List[A] = List.empty[A]): List[A] =
     case Nil => acc
     case head :: tail => reverse(tail, head :: acc)
 
+/* 
 def getJoke()(using Async): String =
-  val backend = CurlBackend()
+  Using( Source.fromURL("https://api.chucknorris.io/jokes/random", Codec.UTF8.name) ) { 
+    source => s"${parseJson( source.mkString )}"
+  }.getOrElse("Chuck Norris is taking a power nap! Come back in a few nanoseconds!")
+ */
+
+def getJoke()(using Async): String =
   val response: Response[String] = quickRequest
-    .get(url)
-    .send(backend)
+    .get("https://api.chucknorris.io/jokes/random")
+    .send( CurlBackend() )
     
     // println( s"*** Now Client response: ${response.body}" )
 
